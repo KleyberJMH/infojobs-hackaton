d3257a36c42ae8ab7f57bd828878c45d9f638973
KleyberJMH <kleyberjmh@gmail.com>
2023-05-25 23:29:17 +0200

    encoded secret

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -5,8 +5,9 @@ const redirectUri = process.env.REDIRECT_URI ?? ''
 export async function POST (request: Request) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code') ?? ''
+  const encodedSecret = encodeURIComponent(infoJobsSecret)
 
-  const res = await fetch(`https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&code=${code}&client_id=${infoJobsId}&client_secret=${infoJobsSecret}&redirect_uri=${redirectUri}`)
+  const res = await fetch(`https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&code=${code}&client_id=${infoJobsId}&client_secret=${encodedSecret}&redirect_uri=${redirectUri}`)
   const data = await res.json()
   console.log({ data })
 }
