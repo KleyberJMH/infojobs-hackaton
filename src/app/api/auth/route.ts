3cd090560ed1b4771fc2c966befb1a38dbb13e5d
KleyberJMH <kleyberjmh@gmail.com>
2023-05-25 23:25:15 +0200

    fix auth

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -6,7 +6,7 @@ export async function POST (request: Request) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code') ?? ''
 
-  const res = await fetch(`https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&client_id=${infoJobsId}&client_secret=${infoJobsSecret}&code=${code}&redirect_uri=${redirectUri}`)
+  const res = await fetch(`https://www.infojobs.net/oauth/authorize?grant_type=authorization_code&code=${code}&client_id=${infoJobsId}&client_secret=${infoJobsSecret}&redirect_uri=${redirectUri}`)
   const data = await res.json()
   console.log({ data })
 }
