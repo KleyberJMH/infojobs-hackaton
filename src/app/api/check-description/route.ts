4500fb5ee30a4d0faf9c1ccea9fdb7a06ec4e16c
KleyberJMH <37642589+KleyberJMH@users.noreply.github.com>
2023-05-27 23:32:30 +0200

    Update route.ts

--- src/app/api/check-description/route.ts
+++ src/app/api/check-description/route.ts
@@ -102,7 +102,7 @@ async function translate (message: string) {
         
           const listOfTranslate = items.map(item => {
             const { translations } = item
-        return { translations }
+        return { translations: translation.text }
               })
           console.log({listOfTranslate})
           return listOfTranslate
