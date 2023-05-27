ad02fbdf0d4a16e037a4d3a54eee8585d057c6b6
KleyberJMH <37642589+KleyberJMH@users.noreply.github.com>
2023-05-27 21:37:58 +0200

    Actualizar route.ts

--- src/app/api/check-description/route.ts
+++ src/app/api/check-description/route.ts
@@ -40,7 +40,7 @@ async function translate (message: string) {
       })
   
       if (response.ok) {
-        console.log(response.translations[0].text)
+        console.log(response[0].translations[0].text)
         const result = await response.json()
         return result
       }
