42b5d18b44969dcdd7bbf595ffdf688c80abe950
Kleyber Mantilla <kleyberjmh@gmail.com>
2023-05-29 18:51:07 +0200

    console.log Data

--- src/app/api/session/cv/route.ts
+++ src/app/api/session/cv/route.ts
@@ -31,6 +31,7 @@ export async function GET (req: NextRequest) {
     }
   })
   const data = await resListCurriculums.json()
+  console.log({ data })
 
   const getPrincipalCurriculum = data.find((curriculum: ICurriculum) => curriculum.principal === true)
 
