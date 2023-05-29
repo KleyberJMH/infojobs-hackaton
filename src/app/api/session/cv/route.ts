0274c24b0c713395c370c7905fdf5a36f0efde43
Kleyber Mantilla <kleyberjmh@gmail.com>
2023-05-29 18:47:24 +0200

    nextauth

--- src/app/api/session/cv/route.ts
+++ src/app/api/session/cv/route.ts
@@ -1,23 +1,54 @@
-import { NextResponse, NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
 
-const infoJobsToken = process.env.INFOJOBS_TOKEN ?? ''
-const nextAuthSecret = process.env.NEXTAUTH_SECRET ?? ''
+const BASE_URL = 'https://api.infojobs.net'
+const CURRICULUM_ENDPOINT = `${BASE_URL}/api/2/curriculum`
+const EXPERIENCE_ENDPOINT = (id: string) => `${BASE_URL}/api/2/curriculum/${id}/experience`
+const CLIENT_ID = process.env.INFOJOBS_ID ?? ''
+const CLIENT_SECRET = process.env.NEXTAUTH_SECRET ?? ''
+const BASIC_TOKEN = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+  'base64'
+)
+
+export interface ICurriculum {
+  id?: number
+  code?: string
+  name?: string
+  principal?: boolean
+  completed?: boolean
+  incompleteSteps?: string[]
+}
 
 export async function GET (req: NextRequest) {
-  const session = await getToken({ req, secret: nextAuthSecret })
-
-  const res = await fetch('https://api.infojobs.net/api/2/curriculum', {
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  if (session == null) {
+    return new NextResponse('Unauthorized', { status: 401 })
+  }
+  const { accessToken } = session
+  const resListCurriculums = await fetch(CURRICULUM_ENDPOINT, {
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${infoJobsToken},Bearer ${session?.accessToken ?? ''}`
+      Authorization: `Basic ${BASIC_TOKEN},Bearer ${accessToken}`
     }
   })
-  const data = await res.json()
-  console.log({ data })
-  try {
-    return NextResponse.json(data)
-  } catch {
-    return new Response('No se pudo acceder a la información', { status: 500 })
+  const data = await resListCurriculums.json()
+
+  const getPrincipalCurriculum = data.find((curriculum: ICurriculum) => curriculum.principal === true)
+
+  if (getPrincipalCurriculum === true) {
+    return new NextResponse('Unauthorized', { status: 401 })
   }
+
+  const curriculum = getPrincipalCurriculum
+
+  const resExperiences = await fetch(EXPERIENCE_ENDPOINT(curriculum.code), {
+    headers: {
+      Authorization: `Basic ${BASIC_TOKEN},Bearer ${accessToken}`
+    }
+  })
+
+  const dataExperience = await resExperiences.json()
+
+  return NextResponse.json({
+    dataExperience
+  })
 }
