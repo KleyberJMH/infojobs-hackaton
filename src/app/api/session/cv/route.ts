dd2eaf48b2e34a370e360f7168c47ceb3456259b
Kleyber Mantilla <kleyberjmh@gmail.com>
2023-05-29 18:19:37 +0200

    nextauht y tipado

--- src/app/api/session/cv/route.ts
+++ src/app/api/session/cv/route.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
 const infoJobsToken = process.env.INFOJOBS_TOKEN ?? ''
+const nextAuthSecret = process.env.NEXTAUTH_SECRET ?? ''
 
-export async function GET (request: Request) {
+export async function GET (req: NextRequest) {
+  const session = await getToken({ req, secret: nextAuthSecret })
+
   const res = await fetch('https://api.infojobs.net/api/2/curriculum', {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Basic ${infoJobsToken}` // sacas el infojobs token, etc.
+      Authorization: `Basic ${infoJobsToken},Bearer ${session?.accessToken ?? ''}`
     }
   })
   const data = await res.json()
