dda4d81242eff371fbd7135456d237442312b1b3
Kleyber Mantilla <kleyberjmh@gmail.com>
2023-05-29 18:40:52 +0200

    add jwt

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -24,12 +24,20 @@ export const authOptions: NextAuthOptions = {
     })
   ],
   callbacks: {
+    async jwt ({ token, account }) {
+      if (account != null) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
     async session ({ session, token }) {
       session.accessToken = token.accessToken
       session.refreshToken = token.refreshToken
       return session
     }
-  }
+  },
+  debug: false
 }
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
